5e66e2bf2a846de489d6a710289ecaf804db9545
Alvaro Suarez Perez <suarezperezalvaro@gmail.com>
2015-09-21 12:24:06 +0200

    retina images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,21 @@ $(window).scroll(function(e){
         header.removeClass('huge');
     }
 });
+
+$(function () {
+	 if (window.devicePixelRatio > 1) {
+         var images = $("img.hires");
+
+         // loop through the images and make them hi-res
+         for(var i = 0; i < images.length; i++) {
+
+           // create new image name
+           var imageType = images[i].src.substr(-4);
+           var imageName = images[i].src.substr(0, images[i].src.length - 4);
+           imageName += "@2x" + imageType;
+
+           //rename image
+           images[i].src = imageName;
+         }
+    }
+});
