010faecbece3f81e55b7edb34e4dbf7bf2fc0eea
Daniel Lombraña González <teleyinex@gmail.com>
2016-01-25 16:42:49 +0100

    Animations.

---
+++ assets/js/anim.sound.svg.js
@@ -0,0 +1,87 @@
+function animSoundCase() {
+    window.animSound = false;
+    var s = Snap("#case-audio");
+    Snap.load("/assets/img/case-audio.svg", function(f) {
+        var time = 500;
+
+        var totalTime = 0;
+
+        var playTwo = f.select("#play-2");
+        var pauseTwo = f.select("#pause-2");
+        var barTwo = f.select("#bar-2");
+        var progressTwo = f.select("#progress-2");
+        var btnBird = f.select("#btn-bird");
+
+
+        btnBird.attr({fill: '#2F4149'});
+        playTwo.attr({opacity: '0'});
+        barTwo.attr({x: 70});
+        progressTwo.attr({width: '30'});
+
+        function animShowWave(id, last, time){
+            //var time = parseInt(Math.random() * 100);
+            if (last) {
+                id.animate({opacity: '1'}, time, mina.easein, function(){
+                    pauseTwo.attr({opacity: '0'});
+                    playTwo.attr({opacity: '1'});
+                });
+            }
+            else {
+                id.animate({opacity: '1'}, time, mina.easein);
+            }
+        }
+
+        function animHideWave(id, last){
+            var time = parseInt(Math.random() * 1000);
+            var timeTwo = 1000 - time;
+            id.animate({opacity: '0'}, time, mina.easeout, function(){
+                animShowWave(id, last, timeTwo);
+            }); 
+        }
+
+        function selectOption(){
+            btnBird.animate({fill: '#077CA0'}, time, mina.easein);
+        }
+
+        var waves = [];
+
+        for(i=4;i<40;i++) {
+            var line = f.select("#Line-Copy-" + i);
+            waves.push(line);
+        }
+
+        function startAnim(){
+
+            if (window.animSound === false) {
+                window.animSound = true;
+                for(i=0;i<waves.length;i++) {
+                    var last = false;
+                    if (i==0) {
+                        barTwo.animate({x: '240'}, 1000,
+                                mina.easein, selectOption);
+                        progressTwo.animate({width: '200'}, 1000, mina.easein);
+                    }
+                    if (i==waves.length) {
+                        last = true;
+                    }
+                    animHideWave(waves[i], last);
+                }
+
+
+            }
+
+        }
+
+
+
+        s.append(f);
+
+        var waypoint = new Waypoint({
+            element: document.getElementById('case-audio-waypoint'),
+            offset: 85,
+            handler: startAnim
+        });
+
+    });          
+};
+
---
+++ assets/js/anim.translation.svg.js
@@ -0,0 +1,107 @@
+function animTranslationCase() {
+    var s = Snap("#case-translation");
+    Snap.load("/assets/img/case-translation.svg", function(f) {
+
+        var time = 500;
+        var card5 = f.select("#user5-card");
+        var user5avatar = f.select("#user5-avatar");
+        var playThree = f.select("#play-3");
+        var pauseThree = f.select("#pause-3");
+        var barThree = f.select("#bar-3");
+        var progressThree = f.select("#progress-3");
+        var user5Line1 = f.select("#user5-line1");
+        var user5Line2 = f.select("#user5-line2");
+        var user5Line3 = f.select("#user5-line3");
+
+        var words = [];
+        for(i=1;i<8;i++) {
+            var word = f.select("#word" + i);
+            words.push(word);
+            word.attr({opacity: '0'});
+        }
+
+        function showWords() {
+        words[0].animate({opacity: '1'}, time, mina.easein, function(){
+            words[1].animate({opacity: '1'}, time, mina.easein, function(){
+                words[2].animate({opacity: '1'}, time, mina.easein, function(){
+                    words[3].animate({opacity: '1'}, time, mina.easein, function(){
+                        words[4].animate({opacity: '1'}, time, mina.easein, function(){
+                            words[5].animate({opacity: '1'}, time, mina.easein, function(){
+                                words[6].animate({opacity: '1'}, time, mina.easein, function(){
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+        }
+
+
+
+        user5Line1.attr({opacity: '0'});
+        user5Line2.attr({opacity: '0'});
+        user5Line3.attr({opacity: '0'});
+
+        playThree.attr({opacity: '0'});
+        barThree.attr({x: '70'});
+        progressThree.attr({width: '30'});
+
+        card5.attr({opacity: '0'});
+        user5avatar.attr({opacity: '0'});
+
+        function showUser5Avatar() {
+            user5avatar.animate({opacity: '1'}, time, mina.easein);
+        }
+
+        function showCard5() {
+            pauseThree.attr({opacity: '0'});
+            playThree.attr({opacity: '1'});
+            card5.animate({opacity: '1'}, time, mina.easein);
+            user5avatar.animate({opacity: '1'}, time, mina.easein, showCardUser5Lines);
+        }
+
+        function showCardUser5Lines(){
+            function showUser5Line2() {
+                user5Line2.animate({opacity: '1'}, 
+                        time, 
+                        mina.easein,
+                        showUser5Line3);
+            }
+
+            function showUser5Line3() {
+                user5Line3.animate({opacity: '1'}, 
+                        time, mina.easein);
+            }
+
+
+            user5Line1.animate({opacity: '1'}, 
+                    time, 
+                    mina.easein, 
+                    showUser5Line2);
+        }
+
+        function showUser1Avatar() {
+            user1avatar.animate({opacity: '1'}, time, mina.easein,
+                    showCardUser1Lines);
+        }
+
+
+
+        function startAnim() {
+            showWords();
+            barThree.animate({x: '240'}, 3500, mina.easein);
+            progressThree.animate({width: '200'}, 3500, mina.easein, showCard5);
+        }
+
+        s.append(f);
+
+        var waypoint = new Waypoint({
+            element: document.getElementById('case-translation-waypoint'),
+            offset: 85,
+            handler: startAnim
+        });
+
+    });          
+};
+
