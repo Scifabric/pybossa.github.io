a3e837a887f8816bfc69e801251d4df09e99d358
Daniel Lombraña González <teleyinex@gmail.com>
2016-01-25 16:58:29 +0100

    Anim photo.

---
+++ assets/js/anim.photo.svg.js
@@ -0,0 +1,27 @@
+function animPhotoCase() {
+    window.animSound = false;
+    var s = Snap("#case-photo");
+    Snap.load("/assets/img/case-classification.svg", function(f) {
+        var time = 500;
+
+        var btn = f.select("#btnNessy");
+
+
+        btn.attr({fill: '#2F4149'});
+
+
+        function startAnimPhoto(){
+            btn.animate({fill: '#077CA0'}, time, mina.easein);
+        }
+
+        s.append(f);
+
+        var waypoint = new Waypoint({
+            element: document.getElementById('case-geo-waypoint'),
+            offset: 85,
+            handler: startAnimPhoto
+        });
+
+    });          
+};
+
